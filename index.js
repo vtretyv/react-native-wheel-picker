d5cbd2026e3b1e0326dd60a9b1fcd71ab60f55f9
vladimir tretyakov <v.tretyakov@partner.samsung.com>
2019-09-23 10:31:04 -0700

    init copy commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+
+import { PickerIOS, Platform } from 'react-native';
+
+import WheelCurvedPicker from './WheelCurvedPicker';
+
+module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker);
